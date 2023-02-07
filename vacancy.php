6b894d635e4e929e0aa164aed4893ea8c9d397f4
markSal <marksalvadore@gmail.com>
2023-02-07 13:46:59 -0600

    Update vacancy.php

--- vacancy.php
+++ vacancy.php
@@ -12,22 +12,24 @@
 		include('head.php');
 	}
 	
-	
 	// Setup dates
 	date_default_timezone_set('America/Chicago');
 	$date_start = date('Y-m-d');
 	
-	// Get Platoon
-	$plt = outputPlatoon(new DateTime($date_start));
+	// Get Platoon for start date
+	$plt = outputPlatoon(new DateTime($date_start));  // Fucntion determines if platoon is A,B,C based on date supplied
 	
-	// Set date range for schedule
+	// Set date range for schedule data request
 	$start_date = date(DATE_ISO8601, strtotime($date_start . ' 07:00:00'));
 	$end_date = date(DATE_ISO8601, strtotime(date('Y-m-d',(strtotime ('+1 day' ,strtotime ($date_start)))) . ' 06:59:59'));
 
-	// Get schedule info
+	// Get schedule data
 	$schedule = getScheduleInfo($auth, $start_date, $end_date);
-
+	
+	// Get assignments from schedule data
 	$assignments = $schedule[0]->assignments;
+
+	// Setup variable to store filtered data
 	$schedule_info = array();
 	$i = 0;
 	
@@ -37,26 +39,29 @@
 		$assignment_name = $assignment->name;
 		$assignment_hours = $assignment->duration_hours;
 		
-		// Loop through positions for assignments
+		// Loop through positions for each assignment
 		foreach($assignment->positions as $position){
 			$assignment_vacancy = $position->is_vacant;
 			
 			// Check if position is vacant
 			if(empty($position->work_shifts[0]->work_type->name) && $assignment_vacancy == 1){
 				
-				// If vacant
+				// If vacant store filtered data in $schedule_info array
 				
-				// Store assignment and position information
+				// Store assignment name
 				$schedule_info[$i]['assignment'] = $assignment_name;
 			
 				// If no district is listed default to "Staff" for district
 				$district = array_key_exists($assignment_name, $company_district_ref) ? $company_district_ref[$assignment_name] : 'Staff';
 				$schedule_info[$i]['district'] = $district;
 				
+				// Store position/rank name
 				$schedule_info[$i]['rank'] = $position->qualifier->name;
+				
+				// Store length of position on schedule
 				$schedule_info[$i]['hours'] = $assignment_hours;
 				
-				// Return blanks for assigned person's information since this is a vacancy 
+				// Return blanks for assigned assigned person's information since this is a vacancy 
 				$schedule_info[$i]['last_name'] = '';
 				$schedule_info[$i]['first_name'] = '';
 				$schedule_info[$i]['full_name'] = '';
@@ -66,18 +71,24 @@
 				$i++;
 			}else{
 				
-				// If not vacant skip this record and go to the next
+				// If not a vacant position, skip this record and go to the next (we filtering out filled positions)
 				continue;
 			}
 		}
 	}
 	
-	// Output CSV file of schedule data if download has been requested
+	// Output CSV file of schedule data if download has been requested instead
 	if($_GET['download'] == 1){
+		
+		// Trigger download
 		output_csv($date_start . '-nofd-schedule-vacancies.csv', $schedule_info);
+		
+		// Don't display rest of report
 		die();
 	}
 	
+	
+	// Output report to browser
 	?>
 	<div class="page-title">
 		<h1>NOFD Schedule Vacancies<span class="smaller"><?php echo date('l, F j, Y', strtotime($date_start));?> </span></h1>
@@ -89,11 +100,6 @@
 	</div>
 	
 	<div class="staffing-dashboard">
-		<?php 
-		// This will probably get moved to config.php
-		$plt = outputPlatoon(new DateTime($date_start)); 
-		?>
-		
 		<div class="plt"><strong>Platoon:</strong> <span class="<?php echo $plt; ?>"><?php echo $plt; ?></span></div>
 	</div>
 	
@@ -113,7 +119,7 @@
 			
 			<tbody>
 				<?php
-				// Display schedule information in table
+				// Display vacancies stored in $schedule_info array in table
 				foreach($schedule_info as $assignment){
 					?>
 					<tr>
